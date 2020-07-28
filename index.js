58b3f4cd959295d3e36ebca59b8fb949dbe2fb2c
Megan Horsfield <meganthadev@gmail.com>
2020-07-28 08:52:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function submitData(name, email) {
+   let formData = {
+        name: name,
+        email: email
+    };
+    
  fetch("http://localhost:3000/users", configObj)
  .then(function(response) {
     return response.json();
