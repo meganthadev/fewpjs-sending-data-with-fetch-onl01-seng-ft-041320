f726566137f5f55c837aeef7ad5e35d11b6bd210
Megan Horsfield <meganthadev@gmail.com>
2020-07-28 08:51:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 function submitData(name, email) {
- 
-};
- 
-let configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
- 
-fetch("http://localhost:3000/dogs", configObj)
-  .then(function(response) {
+ fetch("http://localhost:3000/users", configObj)
+ .then(function(response) {
     return response.json();
   })
   .then(function(object) {
@@ -22,3 +10,13 @@ fetch("http://localhost:3000/dogs", configObj)
     alert("Bad things! Ragnarők!");
     console.log(error.message);
   });
+};
+ 
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(submitData)
+};
