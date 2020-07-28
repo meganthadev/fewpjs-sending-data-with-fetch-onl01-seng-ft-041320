83bff3ee4873e0ff9a9d06457c3d511fd4acdd5e
Megan Horsfield <meganthadev@gmail.com>
2020-07-28 08:42:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-fetch("http://localhost:3000/dogs", {
+let formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle"
+};
+ 
+let configObj = {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
-     "Accept": "application/json"
+    "Accept": "application/json"
   },
-  body: JSON.stringify({
-    dogName: "Byron",
-    dogBreed: "Poodle"
+  body: JSON.stringify(formData)
+};
+ 
+fetch("http://localhost:3000/dogs", configObj)
+  .then(function(response) {
+    return response.json();
   })
-});
+  .then(function(object) {
+    console.log(object);
+  });
