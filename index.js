c68597c0fbdd12b11df84b41f6ec5b64cdccfaaa
Megan Horsfield <meganthadev@gmail.com>
2020-07-28 08:57:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,15 +15,18 @@ function submitData(name, email) {
     
   fetch("http://localhost:3000/users", configObj)
     .then(function(response) {
-      return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  })
-  .catch(function(error) {
-    alert("Error!");
-    console.log(error.message);
-  });
-}
- 
-
+          return response.json();
+        })
+        .then(function(object) {
+            let h2 = document.createElement('h2');
+            h2.innerHTML = object.id;
+            document.body.appendChild(h2);
+            console.log(object);
+        })
+        .catch(function(error) {
+            let h3 = document.createElement('h3');
+            h3.innerHTML = error.message;
+            document.body.appendChild(h3);
+            console.log(error.message);
+        });
+} 
