e8747a3add97c37e1c79e09e66c26799514178f2
Megan Horsfield <meganthadev@gmail.com>
2020-07-28 08:53:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ function submitData(name, email) {
         email: email
     };
     
+  let configObj = {
+    method: "POST",
+    headers: {
+     "Content-Type": "application/json",
+     "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+    
  fetch("http://localhost:3000/users", configObj)
  .then(function(response) {
     return response.json();
@@ -17,11 +26,4 @@ function submitData(name, email) {
   });
 };
  
-let configObj = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
+
